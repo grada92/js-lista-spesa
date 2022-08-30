d08939787b95aad6be9566c78856a5ac10a0f662
grada92 <danielegradassai@gmail.com>
2022-08-30 13:42:45 +0200

    creo ciclo while

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ console.log('js ok');
 
 // Creo un array con lista della spesa
 let list = ['pane', 'pomodori','carne','latte','uova'];
+let i =  0;
+let spesa = document.getElementById('container');
+
+//Creo ciclo while per stampare la lista in console
+while (i < list.length){
+    console.log(list[i]);
+    i++;
+}
 
 
 
